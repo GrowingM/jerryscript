3da3a74f710c7dcadb2d3b2ee40efb192baee0a1
Zoltan Herczeg <zherczeg@inf.u-szeged.hu>
2016-01-03 07:33:33 -0800

    Compact-byte-code representation.
    JerryScript-DCO-1.0-Signed-off-by: Zoltan Herczeg zherczeg@inf.u-szeged.hu

---
+++ jerry-core/parser/js/new-parser/tests/js/new-expression.js
@@ -0,0 +1,27 @@
+/* This test contains new expression parser tests. */
+
+next_statement;
+
+new new a;
+
+next_statement;
+
+new new a(x)(y);
+
+next_statement;
+
+new new a()(x,y)(z);
+
+next_statement;
+
+new new a().b(c);
+
+next_statement;
+
+new new a()[b](c);
+
+next_statement;
+
+new a(x,y,z);
+
+next_statement;
